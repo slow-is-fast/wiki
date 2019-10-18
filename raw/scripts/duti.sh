04db7294a3220d2ace6b85e527a107fb4e8dc349
s4tar <89261073@qq.com>
2019-10-18 10:45:00 +0800

    add duti

---
+++ raw/scripts/duti.sh
@@ -0,0 +1,40 @@
+# Files
+duti -s com.sublimetext.3 .json all
+duti -s com.sublimetext.3 .md all
+duti -s com.sublimetext.3 .markdown all
+duti -s com.sublimetext.3 .txt all
+duti -s com.sublimetext.3 .rtf all
+duti -s com.sublimetext.3 .xml all
+duti -s com.sublimetext.3 .yml all
+duti -s com.sublimetext.3 .yaml all
+
+
+# Web Development
+duti -s com.sublimetext.3 .php all
+duti -s com.sublimetext.3 .phpt all
+duti -s com.sublimetext.3 .stub all
+duti -s com.sublimetext.3 .vue all
+duti -s com.sublimetext.3 .js all
+duti -s com.sublimetext.3 .jsx all
+duti -s com.sublimetext.3 .jxs all
+duti -s com.sublimetext.3 .coffee all
+duti -s com.sublimetext.3 .ts all
+duti -s com.sublimetext.3 .css all
+duti -s com.sublimetext.3 .scss all
+duti -s com.sublimetext.3 .sass all
+duti -s com.sublimetext.3 .less all
+
+
+# Wechat MiniProgram
+duti -s com.sublimetext.3 .wxml all
+duti -s com.sublimetext.3 .wxss all
+
+
+# Other Programming Languages
+duti -s com.sublimetext.3 .go all
+duti -s com.sublimetext.3 .sh all
+duti -s com.sublimetext.3 .py all
+duti -s com.sublimetext.3 .csv all
+duti -s com.sublimetext.3 .rb all
+duti -s com.sublimetext.3 .cpp all
+duti -s com.sublimetext.3 .c all
